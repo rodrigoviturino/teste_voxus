c6ca8c3450de9d4f1196d80fbe5c78165eed3d28
Rodrigo Viturino <Viturino_souza@outlook.com>
2019-06-21 02:13:38 -0300

    Criação do layout em html,css e javascript, de acordo com o frame

--- js/main.js
+++ js/main.js
@@ -1,52 +1,74 @@
-$("input[type='text']").focus(function(){
-    var $this = $(this); $this.parent().find('label').addClass('label-up');
-    });
-    $("input[type='text']").blur(function(){
-      $this=$(this);
-      if(!$this.val()){ $this.parent().find('label').removeClass('label-up');
+// $("input[type='text']").focus(function(){
+//     var $this = $(this); $this.parent().find('label').addClass('label-up');
+//     });
+//     $("input[type='text']").blur(function(){
+//       $this=$(this);
+//       if(!$this.val()){ $this.parent().find('label').removeClass('label-up');
       
-      }
+//       }
+//     });
+
+
+    let form = document.getElementById('form-content');
+    let input = form.querySelectorAll('input');
+    let btn_hidden = document.getElementById('tt');
+    
+    let btn = form.querySelector('#btn-submit');
+        
+    btn.addEventListener('click', function(event){
+        event.preventDefault();
+
+        
+            input.forEach((ss) => {
+                console.log('enviado');                        
+            })
+        
+
+        input.forEach( (a) => {
+            if(a == ''){
+                console.log('vazio');                
+            } else {
+            
+            }
+        });
+        
     });
+    
+    
 
-// Form
-
-// let btn_form = document.querySelector('.btn-form');
-// btn_form.addEventListener('click' , (event) => {
-//     event.preventDefault();
-// });
 
 // validation
-$(document).ready(function () {
-    $('#form-content').validate({
-        // Regras
-        rules: {
-            name: {
-                required:true,
-                minlength:10
-            },
+// $(document).ready(function () {
+//     $('#form-content').validate({
+//         // Regras
+//         rules: {
+//             name: {
+//                 required:true,
+//                 minlength:10
+//             },
 
-            phone: {
-                required: true,
-                minlength: 14,
-                maxlength:20,
-            },
-            email: {
-                email: true,
-                remote:true,
-                required:true
-            },
-            company: {
-                required:true
-            },
-            site: {
-                required:true,
-                url:true
-            },
+//             phone: {
+//                 required: true,
+//                 minlength: 14,
+//                 maxlength:20,
+//             },
+//             email: {
+//                 email: true,
+//                 remote:true,
+//                 required:true
+//             },
+//             company: {
+//                 required:true
+//             },
+//             site: {
+//                 required:true,
+//                 url:true
+//             },
 
-        },
-        submitHandler: function(form) {
+//         },
+//         submitHandler: function(form) {
             
-            form.submit();
-        }
-    });
-});
+//             form.submit();
+//         }
+//     });
+// });
